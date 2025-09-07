27375c7f327b8dd7563f3adbba5a0fca873a5c0a
su469843 <hanghangjason@qq.com>
2025-09-07 20:29:51 +0800

    修复构建问题：添加缺失的导入，解决合并冲突

--- app/src/main/java/io/qzz/studyhard/markdown/network/UpdateChecker.java
+++ app/src/main/java/io/qzz/studyhard/markdown/network/UpdateChecker.java
@@ -14,8 +14,10 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
